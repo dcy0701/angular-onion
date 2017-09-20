ceb81afc324ece8baa9fb7b6bd41c001bd076d4c
dongchongyang <dongchongyang@meituan.com>
2017-09-20 14:04:29 +0800

    feat: 去掉toString转换

--- src/decorators/$observable.js
+++ src/decorators/$observable.js
@@ -22,12 +22,6 @@ const Observable = (...options) => (target, name, descriptor) => {
     // $$Observable Watch 的队列
     target.$$Observable || (target.$$Observable = []);
 
-    // 为什么判断 undefined. 因为babel转码会把es module的this转化为 undefined; 并且格式化
-    if (typeof expression !== 'string' && expression.toString().includes('undefined.')) {
-        let line = expression.toString().split('\n').filter(line => line.includes('return'))[0];
-        expression = line.toString().replace('return', '').replace('undefined.', '$ctrl.');
-    }
-
     let observe = {
         method: watchMethod,
         expression,
